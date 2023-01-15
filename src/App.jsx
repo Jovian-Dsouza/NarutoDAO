aabf611f2e7a2df8d9e0288f44bf2f475b7303fd
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2023-01-15 19:39:09 +0530

    Show off token holders on DAO webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,76 @@
+import { JsonRpcProvider } from '@ethersproject/providers';
 import { useAddress, ConnectWallet, Web3Button, useContract, useNFTBalance } from '@thirdweb-dev/react';
 import { useState, useEffect, useMemo } from "react";
 
 const App = () => {
 
   // Consts
-  const editionDropAddress = "0xCA384f0A630ef90B55601f07D628d07c284772f4";
+  const editionDropAddress = "0xef447f7d9f2a71fd35C6e91752f481723253fC9b";
+  const tokenAddress = "0xfDD014e7FC81FeFCe729C0e058872f91675a4644"
 
   const address = useAddress();
   console.log("Address :", address);
 
   const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  const { contract: token } = useContract(tokenAddress, 'token');
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0");
 
   const hasClaimedNFT = useMemo(() => nftBalance && nftBalance.gt(0),
                                 [nftBalance])
+
+
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => (str.substring(0,6) + '...' + str.substring(str.length-4));
+
+  // This useEffect grabs all the addresses of our members holding our NFT.
+  useEffect(() => {
+    if(!hasClaimedNFT){ return; }
+
+    const getAllAddresses = async () => {
+      try{
+        const memberAddresses = await editionDrop?.history.getAllClaimerAddresses("0");
+        setMemberAddresses(memberAddresses);
+        console.log('🚀 Members addresses', memberAddresses);
+      }
+      catch (error){
+        console.error('Failed to get member list', error);
+      }
+    }
+
+    getAllAddresses();
+  }, [hasClaimedNFT, editionDrop?.history]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+  
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token?.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log('👜 Amounts', amounts);
+      } catch (error) {
+        console.error('failed to get member balances', error);
+      }
+    };
+    getAllBalances();
+  }, [hasClaimedNFT, token?.history]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map(address => {
+      const member = memberTokenAmounts?.find(({holder}) => holder === address);
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || '0'
+      }
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   if(!address){
     return (
@@ -26,14 +83,39 @@ const App = () => {
     );
   }
 
-  if(hasClaimedNFT){
-    return (
-      <div className="landing">
-        <h1>🍪DAO Member Page</h1>
-        <p>Congratulations on being a member</p>
+// If the user has already claimed their NFT we want to display the internal DAO page to them
+// only DAO members will see this. Render all the members + token amounts.
+if (hasClaimedNFT) {
+  return (
+    <div className="member-page">
+      <h1>🍪DAO Member Page</h1>
+      <p>Congratulations on being a member</p>
+      <div>
+        <div>
+          <h2>Member List</h2>
+          <table className="card">
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Token Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {memberList.map((member) => {
+                return (
+                  <tr key={member.address}>
+                    <td>{shortenAddress(member.address)}</td>
+                    <td>{member.tokenAmount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
   
   // Render mint nft screen.
   return (
